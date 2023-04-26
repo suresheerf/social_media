32c2836c37c65e2a7fd5cc6a473a263052351d9d
suresh <suresheerf@gmail.com>
2023-04-26 20:54:19 +0530

    gg

--- config/config.js
+++ config/config.js
@@ -5,7 +5,8 @@ CONFIG.NODE_ENV = process.env.NODE_ENV || 'development';
 CONFIG.PORT = process.env.PORT || 3000;
 CONFIG.JWT_SECRET = process.env.JWT_SECRET;
 CONFIG.JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
-CONFIG.DB_URL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+CONFIG.DB_URL = process.env.DB_URL;
 
 console.log('CONFIG:', CONFIG);
 
