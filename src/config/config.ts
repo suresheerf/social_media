69e34c37f79a766c497e15469d95efd3dd33d747
suresh d <suresheerf@gmail.com>
2024-04-15 17:22:09 +0530

    creating folders

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,15 @@
 import { config } from 'dotenv';
+import fs from 'fs';
 
 config();
+
+const paths = ['public/file', 'logs', 'public/file/test-cases', 'public/file/assertions'];
+const pwd = process.env.PWD || process.cwd();
+paths.forEach((path) => {
+  if (!fs.existsSync(`${pwd}/${path}`)) {
+    fs.mkdirSync(`${pwd}/${path}`, { recursive: true });
+  }
+});
 
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const PORT = process.env.PORT || 3000;
